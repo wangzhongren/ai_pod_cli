49fe4a5d3895e43a61d94bac4e413652c696a982
wangzhongren <510540795@qq.com>
2026-08-30 20:24:12 +0800

    release: publish AIPodCli 0.7.6

--- ai_pod_cli/__init__.py
+++ ai_pod_cli/__init__.py
@@ -1,6 +1,6 @@
 """AIPodCli - AI-native IoC container low-code engine CLI."""
 
-__version__ = "0.7.5"
+__version__ = "0.7.6"
 
 from ai_pod_cli.context import PipelineContext
 from ai_pod_cli.model import Model
